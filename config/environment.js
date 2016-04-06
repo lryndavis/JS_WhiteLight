6a87120381d44dec37fcaff8ca8d88fda47eedf5
Lauryn Davis <lryndavis@gmail.com>
2016-04-06 09:05:19 -0700

    configure firebase with sample seed JSON file

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'white-light',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://white-light-vintage.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
