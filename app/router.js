3ae5062cdc3cac4e32339934f94fd57cdc74350e
Lauryn Davis <lryndavis@gmail.com>
2016-04-06 13:33:07 -0700

    add cart route for viewing shopping cart prior to checkout

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('store');
   this.route('product', {path: '/product/:product_id'});
+  this.route('cart');
 });
 
 export default Router;
