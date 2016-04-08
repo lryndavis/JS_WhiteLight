0dd8cf86e97e3b8038c8970f266a5aa3ff7078cf
Lauryn <lryndavis@gmail.com>
2016-04-08 11:18:49 -0700

    update grandTotal function to convert to readable format on user end

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -4,16 +4,12 @@ export default Ember.Service.extend({
   items: [],
   grandTotal: Ember.computed('items.[]', function(){
     var total = 0;
+    var sign = '$';
     var cartItems = this.get('items');
     for (var i = 0; i < cartItems.length; i++) {
       total += cartItems[i].get('price');
     }
-    return total;
-  }),
-  itemsInCart: Ember.computed('items.[]', function(){
-    var cartItems = this.get('items');
-    var totalItems = cartItems.length;
-    return totalItems;
+    return `${sign}${(Math.floor(total / 100))}.${(total % 100)}`;
   }),
   add(item) {
     this.get('items').pushObject(item);
