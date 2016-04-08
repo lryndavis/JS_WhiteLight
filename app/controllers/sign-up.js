517b3068c81e3e92f924e378fc22f29ce7d9c3eb
Lauryn Davis <lryndavis@gmail.com>
2016-04-08 16:14:07 -0700

    update user auth

--- app/controllers/sign-up.js
+++ app/controllers/sign-up.js
@@ -8,12 +8,16 @@ export default Ember.Controller.extend({
       this.get('firebase').createUser({
         email: this.get('email') || '',
         password: this.get('password') || '',
+        firstName: this.get('firstName') || '',
+        lastName: this.get('lastName') || '',
       }, (error, data) => {
         if (error) {
           console.log(error);
         } else {
           controller.set('email', null);
           controller.set('password', null);
+          controller.set('firstName', null);
+          controller.set('firstName', null);
           controller.transitionToRoute('sign-in');
         }
       });
