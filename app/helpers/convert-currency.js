947a003718164fda84dad1124076b84b42e57eed
Lauryn Davis <lryndavis@gmail.com>
2016-04-10 11:29:08 -0700

    refactor currency conversion helper

--- app/helpers/convert-currency.js
+++ app/helpers/convert-currency.js
@@ -1,14 +1,17 @@
 import Ember from 'ember';
 
 export function convertCurrency(params) {
-  let value = params[0].get('price'),
+  var value = params[0].get('price'),
      dollars = Math.floor(value / 100),
      cents = value % 100,
+     noChange = (value % 100) + '0',
      sign = '$';
 
- if (cents.toString().length === 1) { cents = '0' + cents; }
- return `${sign}${dollars}.${cents}`;
+ if (cents.toString().length === 1) {
+   return `${sign}${dollars}.${noChange}`;
+  } else {
+    return `${sign}${dollars}.${cents}`;
+  }
 }
-
 
 export default Ember.Helper.helper(convertCurrency);
