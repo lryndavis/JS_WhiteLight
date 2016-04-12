5d1a21fd0e556bbe3aead5d054aa2046367ff1ad
Lauryn Davis <lryndavis@gmail.com>
2016-04-12 16:56:22 -0700

    add dropdown menu to navbar

---
+++ app/components/dropdown-menu.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  dropdownOpen: false,
+
+actions: {
+    toggleDropdown() {
+        this.toggleProperty('dropdownOpen');
+    }
+},
+});
