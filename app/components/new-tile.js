3b924a1b555d99ed6abc65b8f5b36e901913cbfe
Lauryn Davis <lryndavis@gmail.com>
2016-04-12 12:15:17 -0700

    add pagination to new products route

--- app/components/new-tile.js
+++ app/components/new-tile.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
+import pagedArray from 'ember-cli-pagination/computed/paged-array';
 
 export default Ember.Component.extend({
   sortDefinition: ['date:asc'],
   sortedProducts: Ember.computed.sort('products', 'sortDefinition'),
+  perPage: 6,
+
+  pagedContent: pagedArray('sortedProducts', {
+    pageBinding: 'page',
+    perPageBinding: 'perPage'
+  }),
 });
