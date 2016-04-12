de39b9acd0ab82ed166a8c59db9d98fe43858009
Lauryn Davis <lryndavis@gmail.com>
2016-04-12 09:24:15 -0700

    begin pagination

---
+++ app/helpers/paginate.js
@@ -0,0 +1,10 @@
+import pagedArray from 'ember-cli-pagination/computed/paged-array';
+
+export default Ember.Object.extend({
+  // The property that contains all objects
+  // In a real app, often set by the route
+  content: model.products,
+
+  // the PagedArray
+  pagedContent: pagedArray('content', {perPage: 6})
+});
---
+++ tests/unit/helpers/paginate-test.js
@@ -0,0 +1,10 @@
+import { paginate } from 'white-light/helpers/paginate';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | paginate');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = paginate([42]);
+  assert.ok(result);
+});
