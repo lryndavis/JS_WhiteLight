7945908768cddcdb3db68b53d534012732469ad6
Lauryn Davis <lryndavis@gmail.com>
2016-04-13 13:56:16 -0700

    add liquid fire transitions to routes

---
+++ app/transitions.js
@@ -0,0 +1,24 @@
+export default function(){
+  this.transition(
+    this.fromRoute('index'),
+    this.toRoute('new'),
+    this.use('fade'),
+    this.reverse('fade')
+  );
+  this.transition(
+    this.fromRoute('index'),
+    this.toRoute('category'),
+    this.use('fade'),
+    this.reverse('fade')
+  );
+    this.transition(
+    this.fromRoute('category'),
+    this.toRoute('product'),
+    this.use('fade')
+  );
+    this.transition(
+    this.fromRoute('new'),
+    this.toRoute('product'),
+    this.use('fade')
+  );
+};
