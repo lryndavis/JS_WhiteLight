70ea2647e77d15631c5a7798f09f5ae173ab1e6c
Lauryn <lryndavis@gmail.com>
2016-04-16 17:28:10 -0700

    add text to about and contact pages

--- app/routes/new.js
+++ app/routes/new.js
@@ -4,10 +4,10 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('product');
   },
-  
+
   actions: {
     sortBy: function(sortProperties) {
       this.set('sortProperties', [sortProperties]);
-    }
+    },
   }
 });
